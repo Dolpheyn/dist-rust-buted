7d069491c5beb24a2d07793d79c37e6dcef02488
Faris Sufyan <47665123+Dolpheyn@users.noreply.github.com>
2023-01-23 09:29:48 +0800

    [dst-pfm-0] Add graceful shutdown step to services (#7)
    Add graceful shutdown step to services

--- src/hello/server.rs
+++ src/hello/server.rs
@@ -1,11 +1,25 @@
-pub mod hello {
+pub mod gen {
     tonic::include_proto!("hello");
 }
 
-use hello::greeter_server::{Greeter, GreeterServer};
-use hello::{SayRequest, SayResponse};
-use tonic::transport::Server;
-use tonic::{Request, Response, Status};
+use std::{convert::Infallible, future::Future, net::SocketAddr};
+
+use http::{Request as HttpRequest, Response as HttpResponse};
+use hyper::Body;
+use tokio::sync::oneshot;
+use tonic::{
+    body::BoxBody,
+    codegen::Service,
+    transport::{NamedService, Server},
+    Request, Response, Status,
+};
+
+use gen::{
+    greeter_server::{Greeter, GreeterServer},
+    SayRequest, SayResponse,
+};
+
+use futures::FutureExt;
 
 #[derive(Debug, Default)]
 pub struct GreeterImpl {}
@@ -26,15 +40,89 @@ impl Greeter for GreeterImpl {
     }
 }
 
+struct ServiceConfig {
+    addr: SocketAddr,
+}
+
+const SERVICE_GROUP: &str = "starter";
+const SERVICE_NAME: &str = "greeter";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
     let greeter = GreeterImpl::default();
+    let service = GreeterServer::new(greeter);
+    let cfg = ServiceConfig {
+        addr: "[::1]:50051".parse()?,
+    };
 
-    Server::builder()
-        .add_service(GreeterServer::new(greeter))
-        .serve(addr)
-        .await?;
+    let register_service = async {
+        // Get SerDict client,
+
+        // Register service
+        println!(
+            "greeter: registering to group.name: {}.{}",
+            SERVICE_GROUP, SERVICE_NAME
+        );
+    };
+    init(register_service).await?;
+
+    let do_shutdown = || {
+        println!("greeter: shutting down...")
+        // Get SerDict client,
+
+        // Deregister service
+    };
+    serve_with_shutdown(service, cfg, do_shutdown).await?;
+
+    Ok(())
+}
+
+async fn init<F>(do_before: F) -> Result<(), Box<dyn std::error::Error>>
+where
+    F: Future<Output = ()>,
+{
+    do_before.await;
+    Ok(())
+}
+
+// TODO: extract to common platform lib
+async fn serve_with_shutdown<S, F>(
+    service: S,
+    cfg: ServiceConfig,
+    on_shutdown: F,
+) -> Result<(), Box<dyn std::error::Error>>
+where
+    F: FnOnce() -> (),
+    S: Service<HttpRequest<Body>, Response = HttpResponse<BoxBody>, Error = Infallible>
+        + NamedService
+        + Clone
+        + Send
+        + 'static,
+    S::Future: Send + 'static,
+{
+    let (shutdown_send, shutdown_recv) = oneshot::channel();
+
+    // Serve server on another task(thread) with a shutdown message channel
+    let server_task = tokio::spawn(async move {
+        Server::builder()
+            .add_service(service)
+            .serve_with_shutdown(cfg.addr, shutdown_recv.map(drop))
+            .await
+            .expect("failed to serve service")
+    });
+
+    // Wait for ctrl_c
+    let _ = tokio::signal::ctrl_c().await;
+
+    println!("dst-pfm: gracefully shutting down server");
+
+    // Send shutdown signal
+    let _ = shutdown_send.send(());
+
+    on_shutdown();
+
+    // Wait for server task to finish exiting
+    server_task.await?;
 
     Ok(())
 }
